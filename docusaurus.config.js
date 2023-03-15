2a0c83365520f62780d08b93746ae7bcf1b83fef
mc_fdc <Masato.04.11.2007@gmail.com>
2023-03-15 12:21:17 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'voicevox-client guide',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'voicevox-client is easy to use voicevox engine.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
