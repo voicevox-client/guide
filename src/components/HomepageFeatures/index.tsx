838e0dfce1bc46aebd8d1372024f526d70b53f92
maamokun <maamo-kun@outlook.com>
2023-04-01 17:51:23 +0900

    more homepage blurb

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -14,28 +14,25 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        voicevox-client is easy to use. Unleash the power of the VOICEVOX engine with so little code!
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Lightweight',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        voicevox-client is built to be lightweight, with minimal impact on performance.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Asynchronous',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        idk
       </>
     ),
   },
