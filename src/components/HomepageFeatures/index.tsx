87604162d50d8f302be3929a1ee2c96a1e1eb991
maamokun <maamo-kun@outlook.com>
2023-04-10 16:59:30 +0900

    test

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,8 +10,8 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Easy to Use, Supports many lanuages',
+    Png: require('@site/static/img/vvclient-languages.png').default,
     description: (
       <>
         voicevox-client is easy to use. Unleash the power of the VOICEVOX engine with so little code!
