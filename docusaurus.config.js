bb4f4ea507e32448700fb3d8eca14eb9a4906638
maamokun <maamo-kun@outlook.com>
2023-05-28 00:11:11 +0900

    skill isue fixed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'Intro',
             position: 'left',
             label: 'Documentation',
           },
