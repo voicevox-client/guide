8c1b752ffe99816410e0be2ffa55723ae9b4fefb
mc_fdc <Masato.04.11.2007@gmail.com>
2023-06-09 09:47:17 +0900

    Change domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://guide.voicevox-client.tuna2134.jp',
+  url: 'https://guide.vclient.tuna2134.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
