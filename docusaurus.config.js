7a656fe3ccae4bad579ed91ec40c4fd30e65c3d9
maamokun <maamo-kun@outlook.com>
2023-06-09 09:37:56 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://guide.voicevox-client.tuna2134.jp',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/guide/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
